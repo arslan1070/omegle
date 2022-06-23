6bf722efe31646496637784dec415428fd284bc7
Arsalan <arsalan30f@gmail.com>
2022-06-23 23:32:11 +0200

    second page is done

--- app.js
+++ app.js
@@ -1,8 +1,27 @@
-var welcomeButton = document.querySelector('.welcome-btn');
 var welcomeContainer = document.querySelector('.welcome-container');
+var secondContainer = document.querySelector('.second-container');
+
+var welcomeButton = document.querySelector('.welcome-btn');
+
+var yesButton = document.querySelector('#yesAnswer');
+var noButton = document.querySelector('#noAnswer');
+
+yesButton.addEventListener('click', function(){
+    yesButton.style.color = 'green';
+    noButton.style.color = 'black';
+})
+noButton.addEventListener('click', function(){
+    noButton.style.color = 'green';
+    yesButton.style.color = 'black';
+})
+
+
+
+
 
 
 
 welcomeButton.addEventListener('click', function(){
     welcomeContainer.style.display = 'none';
+    secondContainer.style.display = 'flex';
 })

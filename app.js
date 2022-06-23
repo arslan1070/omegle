bcc5c753243daefbaabe56d12931b67f25fd4dda
Arsalan <arsalan30f@gmail.com>
2022-06-23 20:57:57 +0200

    welcome page main function done

---
+++ app.js
@@ -0,0 +1,8 @@
+var welcomeButton = document.querySelector('.welcome-btn');
+var welcomeContainer = document.querySelector('.welcome-container');
+
+
+
+welcomeButton.addEventListener('click', function(){
+    welcomeContainer.style.display = 'none';
+})
